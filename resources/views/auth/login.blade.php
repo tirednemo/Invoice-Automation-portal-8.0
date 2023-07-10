f0b18a8ca7f4113803b28280ea930b3d5ac980a6
tirednemo <ummetasnim@iut-dhaka.edu>
2023-07-10 21:43:42 +0600

    register button fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -27,10 +27,7 @@
             <div class="mt-4">
                 <x-label for="password" :value="__('Password')" />
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <!-- Remember Me -->
@@ -42,10 +39,18 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                <x-primary-button type="button">
+                    <a href="{{ route('register') }}">
+                        {{ __('Register') }}
                     </a>
+                </x-primary-button>
+
+                <div class="flex-grow"></div>
+
+                @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
                 @endif
 
                 <x-button class="ml-3">
